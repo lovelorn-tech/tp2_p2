50c099c4225918bc966c8abb9defe8467b44aa9f
lovelorn-tech <118208195+lovelorn-tech@users.noreply.github.com>
2022-11-16 20:46:57 -0300

    Add files via upload

--- co2_mayores_emisores.py
+++ co2_mayores_emisores.py
@@ -11,19 +11,25 @@ def main():
     emisiones_co2_dataframe = obtener_dataframe_emisiones_co2()
 
     top_5_emisores_df = crear_top5_dataframe(emisiones_co2_dataframe, constantes.ULTIMO_ANIO_DISPONIBLE)
-    top_5_emisores_df.plot(kind = 'bar', x = constantes.ARCHIVO_EMISIONES_CO2_COLUMNA_NOMBRES_PAISES, y =constantes.ULTIMO_ANIO_DISPONIBLE)
+    top_5_emisores_df.plot(kind = 'bar', x = constantes.COLUMNA_NOMBRES_PAISES, y =constantes.ULTIMO_ANIO_DISPONIBLE)
+
+    top_5_energias_renovables_df = obtener_dataframe_consumo_energias_renovables(top_5_emisores_df)
+    top_5_energias_renovables_df.plot(kind = 'bar', x = constantes.COLUMNA_NOMBRES_PAISES, y =constantes.ULTIMO_ANIO_DISPONIBLE)
 
     mp.show()
-
-
-def obtener_lista_codigos_paises():
-    codigos_paises = nuevo_dataframe(abrir_archivo(constantes.ARCHIVO_CODIGOS_PAISES))
-    return codigos_paises[constantes.ARCHIVO_CODIGOS_PAISES_COLUMNA_CODIGOS].to_list()
 
 def obtener_dataframe_emisiones_co2():
     codigos_paises = obtener_lista_codigos_paises()
     emisiones_dataframe = nuevo_dataframe(abrir_archivo(constantes.ARCHIVO_EMISIONES_CO2))
-    return filtrar_dataframe(emisiones_dataframe, codigos_paises, constantes.ARCHIVO_EMISIONES_CO2_COLUMNA_CODIGOS_PAISES)
+    return filtrar_dataframe(emisiones_dataframe, codigos_paises, constantes.COLUMNA_CODIGOS_PAISES)
+
+def obtener_dataframe_consumo_energias_renovables(paises): 
+    energias_renovables_dataframe = nuevo_dataframe(abrir_archivo(constantes.ARCHIVO_ENERGIAS_RENOVABLES))
+    return filtrar_dataframe(energias_renovables_dataframe, paises[constantes.COLUMNA_CODIGOS_PAISES].to_list(), constantes.COLUMNA_CODIGOS_PAISES)
+
+def obtener_lista_codigos_paises():
+    codigos_paises = nuevo_dataframe(abrir_archivo(constantes.ARCHIVO_CODIGOS_PAISES))
+    return codigos_paises[constantes.ARCHIVO_CODIGOS_PAISES_COLUMNA_CODIGOS].to_list()
 
 if __name__ == "__main__":
     main()
--- consumidores_renovables.py
+++ consumidores_renovables.py
@@ -8,26 +8,28 @@ import constantes
 
 
 def main():
-    dataframe_energy = get_dataframe(constantes.ARCHIVO_ENERGIAS_RENOVABLES,
-                                     constantes.ARCHIVO_EMISIONES_CO2_COLUMNA_CODIGOS_PAISES)
+    dataframe_energy = get_dataframe(constantes.ARCHIVO_ENERGIAS_RENOVABLES)
+    dataframe_energy = filter_dataframe(dataframe_energy, constantes.COLUMNA_CODIGOS_PAISES, get_country_code_list())
     top5 = crear_top5_dataframe(dataframe_energy, constantes.ULTIMO_ANIO_DISPONIBLE)
-    top5.plot(kind='bar', x=constantes.ARCHIVO_ENERGIAS_RENOVABLES_CODIGO_PAIS, y=constantes.ULTIMO_ANIO_DISPONIBLE)
+    top5.plot(kind='bar', x=constantes.COLUMNA_NOMBRES_PAISES, y=constantes.ULTIMO_ANIO_DISPONIBLE)
 
-
-    dataframe_energy_per_year = get_dataframe(constantes.ARCHIVO_ENERGIAS_RENOVABLES,
-                                              constantes.ARCHIVO_EMISIONES_CO2_COLUMNA_CODIGOS_PAISES)
-    top5_co2 = crear_top5_dataframe(dataframe_energy_per_year, constantes.ULTIMO_ANIO_DISPONIBLE)
-    top5_co2.plot(kind='bar', x=constantes.ARCHIVO_ENERGIAS_RENOVABLES_CODIGO_PAIS, y=constantes.LAST_FIVE_YEARS)
+    dataframe_co2_per_year = get_dataframe(constantes.ARCHIVO_EMISIONES_CO2)
+    country_energy_names = top5[constantes.COLUMNA_NOMBRES_PAISES].tolist()
+    dataframe_co2_per_year = filter_dataframe(dataframe_co2_per_year,
+                                              constantes.COLUMNA_NOMBRES_PAISES,
+                                              country_energy_names)
+    top5_co2 = crear_top5_dataframe(dataframe_co2_per_year, constantes.ULTIMO_ANIO_DISPONIBLE)
+    top5_co2.plot(kind='bar', x=constantes.COLUMNA_NOMBRES_PAISES, y=constantes.LAST_FIVE_YEARS)
 
     mp.show()
 
 
-def get_dataframe(file, filter_file=None):
-    country_code = get_country_code_list()
-    dataframe = nuevo_dataframe(abrir_archivo(file))
-    if filter_file is None:
-        return dataframe
-    return filtrar_dataframe(dataframe, country_code, filter_file)
+def get_dataframe(file):
+    return nuevo_dataframe(abrir_archivo(file))
+
+
+def filter_dataframe(dataframe, filter_column, filter_values):
+    return filtrar_dataframe(dataframe, filter_values, filter_column)
 
 
 def get_country_code_list():
